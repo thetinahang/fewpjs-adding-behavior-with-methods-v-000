b7c49f98036b8135bf7b8b639f8db33aa5626c45
Tina Hang <mstinahang@gmail.com>
2020-02-22 21:16:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,30 @@ class Cat {
     return `${this.name} says meow!`
   }
 }
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    return `${this.name} says woof!`
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    if (this.sex == 'male') {
+      return `It's me! ${this.name}, the parrot!`
+    } else {
+      return `${this.name} says squawk!`
+    }
+
+  }
+}
